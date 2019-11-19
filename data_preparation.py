c60d0d2b6cb6e35682f9c251dc2ed8e89b4cd8a6
Roman <ws.tereshchenko@gmail.com>
2019-11-19 13:14:45 +0300

    !!!!!

--- data_preparation.py
+++ data_preparation.py
@@ -6,4 +6,11 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
 data = pd.read_csv('nocd.csv')
-print(data.head())
+
+data.drop(['STATION', 'NAME'], axis=1, inplace=True)
+data.fillna(data.mean())
+
+data_for_prediction = data.drop('DATE', axis=1)
+
+print(data)
+print(data_for_prediction)
